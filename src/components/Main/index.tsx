fe7b7b367c4796853716417755ab8d813b77987e
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2023-11-20 08:54:32 -0300

    feat: adicionando estilos no component main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,14 @@
-export const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
-)
+"use client"
+
+import * as Styled from './styled'
+export const Main = () => {
+  return(
+  <Styled.Wrapper>
+    <Styled.Logo src="/img/logo.svg" alt='imagem de um átomo e react avançado escrito ao lado'/>
+    <Styled.Title>React Avançado</Styled.Title>
+    <Styled.Description>TypeScript, ReactJs, NextJS e Styled Components</Styled.Description>
+    <Styled.Ilustration src='/img/hero-illustration.svg' alt='Um desenvolvedor de frente para a tela com código'/>
+  </Styled.Wrapper>
+  )
+
+}
