07eb5bffd8e7bfba6b92413381f4409e9af985a1
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2023-11-21 08:53:46 -0300

    feat: adicionando variaveis css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+ :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+    --grey: #cccccc;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
 *{
   margin: 0;
   padding: 0;
@@ -9,6 +22,9 @@ const GlobalStyles = createGlobalStyle`
 
 body {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background: var(--background);
+  color: var(--white);
+
 }
 
 html{
@@ -17,6 +33,15 @@ html{
 
 html, body, #__next{
   height: 100%;
+}
+
+p{
+  font-size: 2rem;
+  line-height:  var(--medium)
+}
+
+a{
+  color: var(--highlight);
 }
 `
 export default GlobalStyles
