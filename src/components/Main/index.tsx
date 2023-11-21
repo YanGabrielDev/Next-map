c6b66bd4fd398deef1f0633c933198cb4c4bf0f9
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2023-11-21 08:52:30 -0300

    fix: revomendo conteudo do component main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,8 @@
 "use client"
 
-import * as Styled from './styled'
 export const Main = () => {
   return(
-  <Styled.Wrapper>
-    <Styled.Logo src="/img/logo.svg" alt='imagem de um átomo e react avançado escrito ao lado'/>
-    <Styled.Title>React Avançado</Styled.Title>
-    <Styled.Description>TypeScript, ReactJs, NextJS e Styled Components</Styled.Description>
-    <Styled.Ilustration src='/img/hero-illustration.svg' alt='Um desenvolvedor de frente para a tela com código'/>
-  </Styled.Wrapper>
+  <h1>asas</h1>
   )
 
 }
