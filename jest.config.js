c78a9d79b7402a96ab17a07fc1049c4883ca900b
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2023-11-23 08:59:56 -0300

    config: configurando jest e o babel para realizar os testes do mapa

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/'],
+  transformIgnorePatterns: [
+    './node_modules/(?!(react-leaflet/lib|@react-leaflet/core/lib)/)'
+  ],
 }
