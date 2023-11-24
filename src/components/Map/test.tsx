5ed6a6f65ee65785f301c28669c6e472cf0b84d2
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2023-11-24 09:00:58 -0300

    test: realizando teste da renderizacao de lugares no mapa

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -5,8 +5,21 @@ describe("<Map />", () => {
   it('should render without any marker', () => {
     render(<Map/>)
     screen.logTestingPlaygroundURL()
+    expect(
+      screen.getByRole('link', { name: /leaflet/i })).toBeInTheDocument()
   })
-  // it("should render with the marker in context place", () => {
+  it("should render with the marker in context place", () => {
+   const place = {
+    id: '1',
+    name: 'Belo Horizonte',
+    slug: 'belo horizonte',
+    location: {
+      latitude: 0,
+      longitude: 0
+    }
+   }
+    render(<Map places={[place]}/>)
 
-  // })
+    expect(screen.getByTitle(/belo horizonte/i)).toBeInTheDocument()
+  })
 })
